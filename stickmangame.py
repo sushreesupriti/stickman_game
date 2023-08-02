27755194d97fb9d12b86593f05d7773dcc9f2831
sushreesupriti <sushreesupriti@gmail.com>
2023-08-02 12:21:03 +0530

    created StickFigure class

--- stickmangame.py
+++ stickmangame.py
@@ -61,9 +61,48 @@ class PlatformSprite(Sprite):
         self.coordinates = Coords(x ,y ,x + width ,y + height)
 
         # super().__init__(game)
-        
 
+class StickFigureSprite(Sprite):
+    def __init__(self, game):
+        # super().__init__(game)
+        Sprite.__init__(self ,game)
+        Sprite.images_left = [
+            PhotoImage(file = "./assets/images/figure-L1.gif"),
+            PhotoImage(file = "./assets/images/figure-L2.gif"),
+            PhotoImage(file = "./assets/images/figure-L3.gif")
+        ]
+        Sprite.images_right = [
+            PhotoImage(file = "./assets/images/figure-R1.gif"),
+            PhotoImage(file = "./assets/images/figure-R2.gif"),
+            PhotoImage(file = "./assets/images/figure-R3.gif")
+        ]
+        self.image = game.canvas.create_image(200 ,470 ,\
+                image = self.image_Left[0] ,anchor='nw')
         
+        self.x = -2
+        self.y = 0
+        self.current_image = 0
+        self.current_image_add = 1
+        self.jump_ount = 0
+        self.last_time = time.time()
+        self.coordinates = Coords()
+        game.canvas.bind_all('<KeyPres-Left>' ,self.turn_left)
+        game.canvas.bind_all('<KeyPres-Right>' ,self.turn_right)
+        game.canvas.bind_all('<KeyPres-space>' ,self.jump)
+
+    def turn_left(self , evt):
+        if self.y == 0:
+            self.x = -2
+        
+    def turn_right(self , evt):
+        if self.y == 0:
+            self.x = 2
+
+    def jump(self , evt):
+        if self.y == 0:
+            self.y = -4
+            self.jump_ount = 0
+
 def within_x(co1 ,co2):
     if (co1.x1 > co2.x1 and co1.x1 < co2.x2)\
             or ( co1.x2 > co2.x1 and co1.x2 < co2.x2 )\
